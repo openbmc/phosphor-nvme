84d430c7b0f890b61b7c672148f6ae38753e599c
Tony Lee <tony.lee@quantatw.com>
2019-06-13 15:26:15 +0800

    Initial commit for phosphor-nvme
    Add initial build files and the main module
    
    Signed-off-by: Tony Lee <tony.lee@quantatw.com>
    Change-Id: I80ed7acc369adee881b70e68c326049056589dcd

---
+++ nvme_manager.hpp
@@ -0,0 +1,43 @@
+#include "config.h"
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <sdeventplus/clock.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/utility/timer.hpp>
+
+namespace phosphor
+{
+namespace nvme
+{
+
+class Nvme
+{
+  public:
+    Nvme() = delete;
+    Nvme(const Nvme&) = delete;
+    Nvme& operator=(const Nvme&) = delete;
+    Nvme(Nvme&&) = delete;
+    Nvme& operator=(Nvme&&) = delete;
+
+    Nvme(sdbusplus::bus::bus& bus) :
+        bus(bus), _event(sdeventplus::Event::get_default()),
+        _timer(_event, std::bind(&Nvme::read, this))
+    {
+    }
+
+    void run();
+
+  private:
+    sdbusplus::bus::bus& bus;
+
+    sdeventplus::Event _event;
+    /** @brief Read Timer */
+    sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic> _timer;
+
+    void init();
+    void read();
+};
+} // namespace nvme
+} // namespace phosphor
