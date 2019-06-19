6d72e77edd536554723ceac97a7a3fc6e6103bec
Tony Lee <tony.lee@quantatw.com>
2019-06-19 10:54:59 +0800

    Add basic function for phosphor-nvme.
    Add basic function to monitor temperature of
    NVMe drive for phosphor-nvme.
    
    Change-Id: Iebe3c8bf4b5cb958defab5b24a88f4bce477ad5e
    Signed-off-by: Tony Lee <tony.lee@quantatw.com>

--- nvme_manager.hpp
+++ nvme_manager.hpp
@@ -1,4 +1,8 @@
+#pragma once
+
 #include "config.h"
+
+#include "nvmes.hpp"
 
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server.hpp>
@@ -12,6 +16,9 @@ namespace phosphor
 namespace nvme
 {
 
+/** @class Nvme
+ *  @brief Nvme manager implementation.
+ */
 class Nvme
 {
   public:
@@ -21,22 +28,75 @@ class Nvme
     Nvme(Nvme&&) = delete;
     Nvme& operator=(Nvme&&) = delete;
 
+    /** @brief Constructs Nvme
+     *
+     * @param[in] bus     - Handle to system dbus
+     * @param[in] objPath - The Dbus path of nvme
+     */
     Nvme(sdbusplus::bus::bus& bus) :
         bus(bus), _event(sdeventplus::Event::get_default()),
         _timer(_event, std::bind(&Nvme::read, this))
     {
     }
 
+    /**
+     * Structure for keeping nvme configure data required by nvme monitoring
+     */
+    struct NVMeConfig
+    {
+        uint8_t index;
+        uint8_t busID;
+        uint8_t presentPin;
+        uint8_t pwrGoodPin;
+        uint64_t criticalHigh;
+        uint64_t criticalLow;
+        uint64_t maxValue;
+        uint64_t minValue;
+        uint64_t warningHigh;
+        uint64_t warningLow;
+    };
+
+    /**
+     * Structure for keeping nvme data required by nvme monitoring
+     */
+    struct NVMeData
+    {
+        bool present;              /* Whether or not the nvme is present  */
+        std::string vendor;        /* The nvme manufacturer  */
+        std::string serialNumber;  /* The nvme serial number  */
+        std::string smartWarnings; /* Indicates smart warnings for the state  */
+        std::string statusFlags;   /* Indicates the status of the drives  */
+        std::string
+            driveLifeUsed; /* A vendor specific estimate of the percentage  */
+        u_int64_t sensorValue; /* Sensor value, if sensor value didn't be
+                                  update, means sensor failure, default set to
+                                  129(0x81) accroding to NVMe-MI SPEC*/
+    };
+
+    /** @brief Setup polling timer in a sd event loop and attach to D-Bus
+     *         event loop.
+     */
     void run();
 
-  private:
-    sdbusplus::bus::bus& bus;
+    /** @brief Get GPIO value of nvme by sysfs */
+    std::string getGPIOValueOfNvme(const std::string& fullPath);
+    /** @brief Map of the object NvmeSSD */
+    std::unordered_map<std::string, std::shared_ptr<phosphor::nvme::NvmeSSD>>
+        nvmes;
 
+  private:
+    /** @brief sdbusplus bus client connection. */
+    sdbusplus::bus::bus& bus;
+    /** @brief the Event Loop structure */
     sdeventplus::Event _event;
     /** @brief Read Timer */
     sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic> _timer;
 
+    std::vector<phosphor::nvme::Nvme::NVMeConfig> configs;
+
+    /** @brief Set up initial configuration value of NVMe */
     void init();
+    /** @brief Monitor NVMe drives every one second  */
     void read();
 };
 } // namespace nvme
---
+++ nvmes.cpp
@@ -0,0 +1,45 @@
+#include "nvmes.hpp"
+
+namespace phosphor
+{
+namespace nvme
+{
+
+void NvmeSSD::checkSensorThreshold()
+{
+    uint64_t value = ValueIface::value();
+    uint64_t criticalHigh = CriticalInterface::criticalHigh();
+    uint64_t criticalLow = CriticalInterface::criticalLow();
+    uint64_t warningHigh = WarningInterface::warningHigh();
+    uint64_t warningLow = WarningInterface::warningLow();
+
+    CriticalInterface::criticalAlarmHigh(value > criticalHigh);
+
+    CriticalInterface::criticalAlarmLow(value < criticalLow);
+
+    WarningInterface::warningAlarmHigh(value > warningHigh);
+
+    WarningInterface::warningAlarmLow(value < warningLow);
+}
+
+void NvmeSSD::setSensorThreshold(uint64_t warningHigh, uint64_t warningLow,
+                                 uint64_t criticalHigh, uint64_t criticalLow,
+                                 uint64_t maxValue, uint64_t minValue)
+{
+    WarningInterface::warningHigh(warningHigh);
+    WarningInterface::warningLow(warningLow);
+
+    CriticalInterface::criticalHigh(criticalHigh);
+    CriticalInterface::criticalLow(criticalLow);
+
+    ValueIface::maxValue(maxValue);
+    ValueIface::minValue(minValue);
+}
+
+void NvmeSSD::setSensorValueToDbus(const u_int64_t value)
+{
+    ValueIface::value(value);
+}
+
+} // namespace nvme
+} // namespace phosphor
