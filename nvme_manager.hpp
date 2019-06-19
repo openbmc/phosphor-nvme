f81df34a5b0228f266fb80ccbf945c4b1e935d8d
Tony Lee <tony.lee@quantatw.com>
2019-06-19 10:54:59 +0800

    Add basic function for phosphor-nvme.
    Add basic function to monitor temperature of
    NVMe drive for phosphor-nvme.
    
    Change-Id: Iebe3c8bf4b5cb958defab5b24a88f4bce477ad5e
    Signed-off-by: Tony Lee <tony.lee@quantatw.com>

--- nvme_manager.hpp
+++ nvme_manager.hpp
@@ -1,5 +1,7 @@
 #include "config.h"
 
+#include "nvmes.hpp"
+
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server.hpp>
 #include <sdbusplus/server/object.hpp>
@@ -12,6 +14,9 @@ namespace phosphor
 namespace nvme
 {
 
+/** @class Nvme
+ *  @brief Nvme manager implementation.
+ */
 class Nvme
 {
   public:
@@ -21,23 +26,72 @@ class Nvme
     Nvme(Nvme&&) = delete;
     Nvme& operator=(Nvme&&) = delete;
 
+    /** @brief Constructs Nvme
+     *
+     * @param[in] bus     - Handle to system dbus
+     * @param[in] objPath - The Dbus path of nvme
+     */
     Nvme(sdbusplus::bus::bus& bus, const char* objPath) :
         bus(bus), _event(sdeventplus::Event::get_default()),
         _timer(_event, std::bind(&Nvme::read, this)), _objPath(objPath)
     {
     }
 
+    /**
+     * Structure for keeping nvme configure data required by nvme monitoring
+     */
+    struct NVMeConfig
+    {
+        uint8_t index;
+        uint8_t busID;
+        uint8_t presentPin;
+        uint8_t pwrGoodPin;
+        uint64_t criticalHigh;
+        uint64_t criticalLow;
+        uint64_t maxValue;
+        uint64_t minValue;
+    };
+
+    /**
+     * Structure for keeping nvme data required by nvme monitoring
+     */
+    struct NVMeData
+    {
+        bool present;              /* Whether or not the nvme is present  */
+        std::string vendor;        /* The nvme manufacturer  */
+        std::string serialNumber;  /* The nvme serial number  */
+        std::string smartWarnings; /* Indicates smart warnings for the state  */
+        std::string statusFlags;   /* Indicates the status of the drives  */
+        std::string
+            driveLifeUsed; /* A vendor specific estimate of the percentage  */
+        u_int64_t sensorValue; /* Sensor value, if sensor value didn't be
+                                  update, means sensor failure, default set to
+                                  129(0x81) accroding to NVMe-MI SPEC*/
+    };
+
+    /** @brief Setup polling timer in a sd event loop and attach to D-Bus
+     *         event loop.
+     */
     void run();
     const std::string _objPath;
 
-  private:
-    sdbusplus::bus::bus& bus;
+    /** @brief Get GPIO value of nvme by sysfs */
+    std::string getGPIOValueOfNvme(std::string);
+    /** @brief Map of the object NvmeSSD */
+    std::unordered_map<std::string, std::shared_ptr<phosphor::nvme::NvmeSSD>>
+        nvmes;
 
+  private:
+    /** @brief sdbusplus bus client connection. */
+    sdbusplus::bus::bus& bus;
+    /** @brief the Event Loop structure */
     sdeventplus::Event _event;
     /** @brief Read Timer */
     sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic> _timer;
 
+    /** @brief Set up initial configuration value of NVMe */
     void init();
+    /** @brief Monitor NVMe drives every one second  */
     void read();
 };
 } // namespace nvme
