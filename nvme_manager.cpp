9ef917e83d393fda5975f38154f557472a7ae91c
Tony Lee <tony.lee@quantatw.com>
2019-06-19 10:54:59 +0800

    Add basic function for phosphor-nvme.
    Add basic function to monitor temperature of
    NVMe drive for phosphor-nvme.
    
    Change-Id: Iebe3c8bf4b5cb958defab5b24a88f4bce477ad5e
    Signed-off-by: Tony Lee <tony.lee@quantatw.com>

--- nvme_manager.cpp
+++ nvme_manager.cpp
@@ -1,9 +1,32 @@
 #include "nvme_manager.hpp"
 
+#include "nlohmann/json.hpp"
+#include "smbus.hpp"
+
+#include <experimental/filesystem>
+#include <fstream>
+#include <iostream>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
+#include <sstream>
+#include <string>
+
+#include "i2c-dev.h"
 
 #define MONITOR_INTERVAL_SECONDS 1
+#define MAX_I2C_BUS 30
+#define NVME_SSD_SLAVE_ADDRESS 0x6a
+#define GPIO_BASE_PATH "/sys/class/gpio/gpio"
+#define IS_PRESENT "0"
+#define POWERGD "1"
+
+static constexpr auto configFile = "/etc/nvme/nvme_config.json";
+auto retries = 3;
+static constexpr auto delay = std::chrono::milliseconds{100};
+using Json = nlohmann::json;
+std::vector<phosphor::nvme::Nvme::NVMeConfig> configs;
+namespace fs = std::experimental::filesystem;
+
 namespace phosphor
 {
 namespace nvme
@@ -11,6 +34,89 @@ namespace nvme
 
 using namespace std;
 using namespace phosphor::logging;
+
+std::string intToHex(int input)
+{
+    std::stringstream tmp;
+    tmp << std::hex << input;
+
+    return tmp.str();
+}
+
+/** @brief Get NVMe info over smbus  */
+bool getNVMeInfobyBusID(int busID, phosphor::nvme::Nvme::NVMeData& nvmeData)
+{
+    nvmeData.present = true;
+    nvmeData.vendor = "";
+    nvmeData.serialNumber = "";
+    nvmeData.smartWarnings = "";
+    nvmeData.statusFlags = "";
+    nvmeData.driveLifeUsed = "";
+    nvmeData.sensorValue = 129;
+
+    phosphor::smbus::Smbus smbus;
+
+    unsigned char rsp_data_command_0[8] = {0};
+    unsigned char rsp_data_command_8[24] = {0};
+
+    // command code
+    uint8_t in_data = 0;
+
+    auto init = smbus.smbusInit(busID);
+
+    if (init == -1)
+    {
+        log<level::ERR>("smbusInit fail!");
+
+        nvmeData.present = false;
+
+        return nvmeData.present;
+    }
+
+    auto res_int = smbus.SendSmbusRWBlockCmdRAW(
+        busID, NVME_SSD_SLAVE_ADDRESS, &in_data, 1, rsp_data_command_0);
+
+    if (res_int < 0)
+    {
+        log<level::ERR>("Send command code 0 fail!");
+
+        smbus.smbusClose(busID);
+        nvmeData.present = false;
+        return nvmeData.present;
+    }
+
+    // command code
+    in_data = 8;
+
+    res_int = smbus.SendSmbusRWBlockCmdRAW(busID, NVME_SSD_SLAVE_ADDRESS,
+                                           &in_data, 1, rsp_data_command_8);
+
+    if (res_int < 0)
+    {
+        log<level::ERR>("Send command code 8 fail!");
+        smbus.smbusClose(busID);
+        nvmeData.present = false;
+        return nvmeData.present;
+    }
+
+    nvmeData.vendor =
+        intToHex(rsp_data_command_8[1]) + " " + intToHex(rsp_data_command_8[2]);
+
+    // offset: serialNumber position
+    for (int offset = 3; offset < 23; offset++)
+    {
+        nvmeData.serialNumber += static_cast<char>(rsp_data_command_8[offset]);
+    }
+
+    nvmeData.statusFlags = intToHex(rsp_data_command_0[1]);
+    nvmeData.smartWarnings = intToHex(rsp_data_command_0[2]);
+    nvmeData.driveLifeUsed = intToHex(rsp_data_command_0[4]);
+    nvmeData.sensorValue = (u_int64_t)rsp_data_command_0[3];
+
+    smbus.smbusClose(busID);
+
+    return nvmeData.present;
+}
 
 void Nvme::run()
 {
@@ -24,17 +130,206 @@ void Nvme::run()
     }
     catch (const std::exception& e)
     {
-        log<level::ERR>("Error in polling loop. "),
+        log<level::ERR>("Error in polling loop."),
             entry("ERROR = %s", e.what());
     }
 }
 
-void Nvme::init()
+/** @brief Parsing NVMe config JSON file  */
+Json parseSensorConfig()
 {
+    std::ifstream jsonFile(configFile);
+    if (!jsonFile.is_open())
+    {
+        log<level::ERR>("NVMe config JSON file not found");
+    }
+
+    auto data = Json::parse(jsonFile, nullptr, false);
+    if (data.is_discarded())
+    {
+        log<level::ERR>("NVMe config readings JSON parser failure");
+    }
+
+    return data;
 }
 
+/** @brief Obtain the initial configuration value of NVMe  */
+std::vector<phosphor::nvme::Nvme::NVMeConfig> getNvmeConfig()
+{
+
+    phosphor::nvme::Nvme::NVMeConfig nvmeConfig;
+    std::vector<phosphor::nvme::Nvme::NVMeConfig> nvmeConfigs;
+    uint64_t criticalHigh = 0;
+    uint64_t criticalLow = 0;
+    uint64_t maxValue = 0;
+    uint64_t minValue = 0;
+
+    try
+    {
+        auto data = parseSensorConfig();
+        static const std::vector<Json> empty{};
+        std::vector<Json> readings = data.value("config", empty);
+        std::vector<Json> thresholds = data.value("threshold", empty);
+        if (!thresholds.empty())
+        {
+            for (const auto& instance : thresholds)
+            {
+                criticalHigh = instance.value("criticalHigh", 0);
+                criticalLow = instance.value("criticalLow", 0);
+                maxValue = instance.value("maxValue", 0);
+                minValue = instance.value("minValue", 0);
+            }
+        }
+        else
+        {
+            log<level::ERR>(
+                "Invalid NVMe config file, thresholds dosen't exist");
+        }
+
+        if (!readings.empty())
+        {
+            for (const auto& instance : readings)
+            {
+                uint8_t index = instance.value("NvmeDriveIndex", 0);
+                uint8_t busID = instance.value("NVMeDriveBusID", 0);
+                uint8_t presentPin = instance.value("NVMeDrivePresentPin", 0);
+                uint8_t pwrGoodPin = instance.value("NVMeDrivePwrGoodPin", 0);
+
+                nvmeConfig.index = index;
+                nvmeConfig.busID = busID;
+                nvmeConfig.presentPin = presentPin;
+                nvmeConfig.pwrGoodPin = pwrGoodPin;
+                nvmeConfig.criticalHigh = criticalHigh;
+                nvmeConfig.criticalLow = criticalLow;
+                nvmeConfig.maxValue = maxValue;
+                nvmeConfig.minValue = minValue;
+                nvmeConfigs.push_back(nvmeConfig);
+            }
+        }
+        else
+        {
+            log<level::ERR>("Invalid NVMe config file, config dosen't exist");
+        }
+    }
+    catch (const Json::exception& e)
+    {
+        log<level::ERR>("Json Exception caught."), entry("MSG: %s", e.what());
+    }
+
+    return nvmeConfigs;
+}
+
+std::string Nvme::getGPIOValueOfNvme(std::string fullPath)
+{
+    std::string val;
+    std::ifstream ifs;
+
+    while (true)
+    {
+        try
+        {
+            if (!ifs.is_open())
+                ifs.open(fullPath);
+            ifs.clear();
+            ifs.seekg(0);
+            ifs >> val;
+        }
+        catch (const std::exception& e)
+        {
+            --retries;
+            std::this_thread::sleep_for(delay);
+            log<level::ERR>("Can not open gpio path."),
+                entry("MSG: %s", e.what());
+            continue;
+        }
+        break;
+    }
+
+    ifs.close();
+    return val;
+}
+
+void Nvme::init()
+{
+    // read json file
+    configs = getNvmeConfig();
+}
+
+/** @brief Monitor NVMe drives every one second  */
 void Nvme::read()
 {
+    std::string devPresentPath;
+    std::string devPwrGoodPath;
+
+    for (int i = 0; i < (int)(configs.size()); i++)
+    {
+        NVMeData nvmeData;
+        devPresentPath =
+            GPIO_BASE_PATH + std::to_string(configs[i].presentPin) + "/value";
+
+        devPwrGoodPath =
+            GPIO_BASE_PATH + std::to_string(configs[i].pwrGoodPin) + "/value";
+
+        auto iter = nvmes.find(std::to_string(configs[i].index));
+
+        if (getGPIOValueOfNvme(devPresentPath) == IS_PRESENT)
+        {
+            // Drive status is good, update value or create d-bus and update
+            // value.
+            if (getGPIOValueOfNvme(devPwrGoodPath) == POWERGD)
+            {
+                // get NVMe information through i2c by busID.
+                getNVMeInfobyBusID(configs[i].busID, nvmeData);
+
+                // can not find. create dbus
+                if (iter == nvmes.end())
+                {
+                    log<level::INFO>("SSD plug."),
+                        entry("index = %s",
+                              std::to_string(configs[i].index).c_str());
+
+                    std::string objPath =
+                        NVME_OBJ_PATH + std::to_string(configs[i].index);
+                    auto nvmeSSD = std::make_shared<phosphor::nvme::NvmeSSD>(
+                        bus, objPath.c_str());
+                    nvmes.emplace(std::to_string(configs[i].index), nvmeSSD);
+
+                    nvmeSSD->setSensorValueToDbus(nvmeData.sensorValue);
+                    nvmeSSD->setSensorThreshold(
+                        configs[i].criticalHigh, configs[i].criticalLow,
+                        configs[i].maxValue, configs[i].minValue);
+
+                    nvmeSSD->checkSensorThreshold();
+                }
+                else
+                {
+                    iter->second->setSensorValueToDbus(nvmeData.sensorValue);
+                    iter->second->checkSensorThreshold();
+                }
+            }
+            else
+            {
+                // Present pin is true but power good pin is false
+                // remove nvme d-bus path
+
+                log<level::ERR>(
+                    "Present pin is true but power good pin is false."),
+                    entry("index = %s",
+                          std::to_string(configs[i].index).c_str());
+                nvmeData = NVMeData();
+                nvmes.erase(std::to_string(configs[i].index));
+                log<level::INFO>("Erase SSD from map and d-bus."),
+                    entry("index = %s",
+                          std::to_string(configs[i].index).c_str());
+            }
+        }
+        else
+        {
+            // Drive not present, remove nvme d-bus path
+            nvmeData = NVMeData();
+            nvmes.erase(std::to_string(configs[i].index));
+        }
+    }
 }
 } // namespace nvme
 } // namespace phosphor
